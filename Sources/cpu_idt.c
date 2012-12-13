6ebcb7fb0552121666cbac807f21add906c39614
Clement Deschamps <clement.deschamps@imag.fr>
2012-12-13 14:08:58 +0100

    fixed IDT creation routines

--- Sources/cpu_idt.c
+++ Sources/cpu_idt.c
@@ -3,31 +3,66 @@
 
 #include "i8259.h"
 
+#define KERNEL_CS 0x10
+
+#define IDT_ENTRIES 256
+extern unsigned long long idt[IDT_ENTRIES];
+extern struct pseudo_descriptor idt_desc;
+
 struct pseudo_descriptor {
     unsigned short limit;
     unsigned long linear_base;
 } __attribute__ ((aligned(2), packed));
 
-#define _set_gate(gate_addr, type, dpl, addr)           \
-    __asm__ __volatile__ ( "movw %%dx, %%ax\n\t"        \
-            "movw %0, %%dx\n\t"                         \
-            "movl %%eax, %1\n\t"                        \
-            "movl %%edx, %2"                            \
-            ::                                          \
-            "i" ((short) (0x8000+(dpl<<13)+(type<<8))), \
-            "o" (*((char *) (gate_addr))),              \
-            "o" (*(4+(char *) (gate_addr))),            \
-            "d" ((char *) (addr)),"a" (KERNEL_CS << 16))
+enum {
+  GATE_INTERRUPT = 0xE,
+  GATE_TRAP = 0xF,
+  GATE_CALL = 0xC,
+  GATE_TASK = 0x5,
+};
 
-#define set_intr_gate(n, addr) _set_gate(&idt[n], 14, 0, addr)
+struct desc_struct {
+  union {
+    struct {
+      unsigned int a;
+      unsigned int b;
+    };
+    struct {
+      uint16_t limit0;
+      uint16_t base0;
+      unsigned base1: 8, type: 4, s: 1, dpl: 2, p: 1;
+      unsigned limit: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
+    };
+  };
+} __attribute__((packed));
 
-#define set_trap_gate(n, addr) _set_gate(&idt[n], 15, 0, addr)
+typedef struct desc_struct gate_desc;
 
-#define set_system_gate(n, addr) _set_gate(&idt[n], 15, 3, addr)
+static inline void write_idt_entry(gate_desc *idt, int entry, const gate_desc *gate) {
+      memcpy(&idt[entry], gate, sizeof(*gate));
+}
 
-#define IDT_ENTRIES 256
-extern unsigned long long idt[IDT_ENTRIES];
-extern struct pseudo_descriptor idt_desc;
+typedef struct desc_struct gate_desc;
+
+static inline void pack_gate(gate_desc *gate, unsigned char type,
+           unsigned long base, unsigned dpl, unsigned flags,
+           unsigned short seg) {
+  gate->a = (seg << 16) | (base & 0xffff);
+  gate->b = (base & 0xffff0000) | (((0x80 | type | (dpl << 5)) & 0xff) << 8);
+}
+
+static inline void _set_gate(int gate, unsigned type, void *addr,
+            unsigned dpl, unsigned ist, unsigned seg) {
+    gate_desc s;
+
+    pack_gate(&s, type, (unsigned long)addr, dpl, ist, seg);
+
+    write_idt_entry((void *) idt, gate, &s);
+}
+
+static inline void set_intr_gate(unsigned int n, void *addr) {
+    _set_gate(n, GATE_INTERRUPT, addr, 0, 0, KERNEL_CS);
+}
 
 extern void divide_err(void);
 extern void illegal_opcode(void);
@@ -35,19 +70,20 @@ extern void protection_fault(void);
 extern void timer_int(void);
 extern void ipi_int(void);
 extern void lapic_error_int(void);
-
-#define KERNEL_CS 0x10
+extern void ignore_int(void);
 
 #define LOCAL_TIMER_VECTOR 0XEF
 #define LAPIC_ERROR_VECTOR 0xEE
 #define LAPIC_IPI_VECTOR 0xED
 
-extern unsigned long i8259_irq[16];
+extern void *i8259_irq[16];
 
 void cpu_idt_init(void) {
     int i;
 
-    memset(idt, 0, sizeof(idt));
+    for(i=0; i<IDT_ENTRIES; i++) {
+        set_intr_gate(i, &ignore_int);    
+    }
 
     set_intr_gate(0, &divide_err);
     set_intr_gate(6, &illegal_opcode);
