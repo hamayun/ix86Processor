f2d8bcef877a3acb74b599e74f1f3439cab44820
Nicolas Fournel <nicolas.fournel@imag.fr>
2013-02-04 16:56:41 +0100

    Fix context save and restore

--- Headers/Public/Processor/Context.h
+++ Headers/Public/Processor/Context.h
@@ -6,8 +6,10 @@
 
 //#define DEBUG_PROCESSOR_CONTEXT
 
-typedef volatile uintptr_t cpu_context_t [32];
-#define CPU_CONTEXT_SIZE sizeof(cpu_context_t)
+#define CPU_CONTEXT_NBREGS 32
+
+typedef volatile uintptr_t cpu_context_t [CPU_CONTEXT_NBREGS];
+#define CPU_CONTEXT_SIZE 128
 
 void cpu_context_init (cpu_context_t * ctx, void * sp, int32_t ssize, void * entry, void * arg);
 void cpu_context_save (cpu_context_t * ctx, uint32_t *entry);
@@ -24,24 +26,27 @@ void cpu_context_save (cpu_context_t * ctx, uint32_t *entry);
 */
 #define cpu_context_save(ctx,addr) \
     do{ \
-        __asm__ volatile (\
-            "push %1 \n"\
-            "push %0 \n"\
-            "push %%ebp \n"\
-            "mov %%esp, %%ebp \n"\
-            "\n"\
-            "mov 0x4(%%ebp), %%esp \n" /* switch to ctx*/\
-            "push (%%ebp) \n"        /* ebp at the beginning of this macro*/ \
-            "push 0x8(%%ebp) \n"     /* address at which we should return*/ \
-            "push %%ebp \n"          /* current position of the ebp*/ \
-            "pushf \n"\
-            "pusha \n"\
-            "mov 0x4(%%ebp), %%eax \n"\
-            "mov %%esp, (%%eax) \n"   /* Update to first entry of context (the push/pop location)*/\
-            "\n"\
-            "leave\n"\
-            "add $0x8, %%esp"\
-        :: "r" ((unsigned long) (ctx)), "r" ((unsigned long) (addr)):"%eax");\
+		 __asm__ volatile (	                                      \
+			"push %1 \n"									      \
+           "push %0 \n"                                           \
+           "push %%ebp \n"                                        \
+           "mov %%esp, %%ebp \n"                                  \
+           "\n"                                                   \
+           "mov 0x4(%%ebp), %%esp \n"       /* switch to ctx  */  \
+           "add $128, %%esp\n" /* move to the end */ \
+           "\n"                             /* for push use    */ \
+           "push (%%ebp) \n"          /* ebp            */        \
+           "push 0x8(%%ebp) \n"       /* return address */        \
+           "push %%ebp \n"            /* esp            */        \
+           "pushf \n"                 /* flags (4B)     */        \
+           "pusha \n"                 /* 8 * 4B         */        \
+           "mov 0x4(%%ebp), %%eax \n" /* eax <- ctxt    */        \
+           "mov %%esp, (%%eax) \n"    /* ctxt[0] <- start */      \
+           "\n"                                                   \
+		   "mov %%ebp, %%esp \n"      /* restore esp    */        \
+		   "pop %%ebp \n"             /* restore ebp    */        \
+           "add $0x8, %%esp"          /* pop useless    */	      \
+           :: "a" ((unsigned long) (ctx)), "b" ((unsigned long) (addr)):);   \
     } while (0)
 
 /*
@@ -50,23 +55,21 @@ void cpu_context_load (cpu_context_t * ctx);
 #define cpu_context_load(ctx) \
     do{ \
         __asm__ volatile (\
-            "mov %0, %%esp \n"   /*switch to ctx*/\
-            "\n"\
-            "pop %%esp \n"  /* This moves the esp to the correct poping location */\
-            "popa \n"\
-            "popf \n"\
-            "pop %%ebp \n"\
-            "mov 0x4(%%esp), %%eax \n"  /* get the ebp_o of save_context into eax */\
-            "mov %%eax, (%%ebp) \n"\
-            "mov (%%esp), %%eax \n"     /* get the return address */\
-            "mov %%eax, 0x04(%%ebp) \n"\
-            "mov %%esp, %%eax\n"      /* Update the push/pop address (The first entry of context)*/\
-            "add $0x08, %%eax\n"\
-            "mov %%eax, %0\n"\
-            "\n"\
-            "leave \n"\
-            "ret $0x4 \n"\
-        ::"r" ((unsigned long) (ctx)):"%eax");\
+            "mov (%0), %%esp \n"        /* esp <- ctxt[0] (== start) */ \
+            "popa \n"                   /* 8 * 4B                    */ \
+            "popf \n"                   /* flags 4B                  */	\
+            "pop %%ebp \n"              /* esp                       */	\
+            "mov 0x4(%%esp), %%eax \n"  /* eax <- ebp                */ \
+            "mov %%eax, (%%ebp) \n"     /* ebp in stack              */ \
+            "mov (%%esp), %%eax \n"     /* eax <- return address     */ \
+		    "mov %%eax, 0x04(%%ebp) \n" /* return address in stack   */ \
+            "mov %%esp, %%eax\n"        /* restore eax               */ \
+            "add $0x08, %%eax\n"                                        \
+		    "sub $128, %%eax \n"                           \
+            "\n"                                                        \
+            "leave \n"                                                  \
+            "ret $0x4 \n"                                               \
+        ::"a" ((unsigned long) (ctx)):);\
     } while (0)
 
 #endif
