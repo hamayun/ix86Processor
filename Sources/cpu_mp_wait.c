39f57ec6c0d4818eee4285126b66f2992e77df12
Mian M. Hamayun <mian-muhammad.hamayun@imag.fr>
2013-03-09 19:46:44 +0100

    Added I/O Request for SystemC Sleep

--- Sources/cpu_mp_wait.c
+++ Sources/cpu_mp_wait.c
@@ -1,9 +1,11 @@
 #include <Processor/Processor.h>
 
+// Tell SystemC CPU to go on Wait
+#define MP_SYNCHRO_WAIT 0		// 0 means put myself on wait 
+
 void cpu_mp_wait (void)
 {
   dna_printf("CPU-%d in cpu_mp_wait\n", cpu_mp_id());
-
   cpu_mp_synchro = 1;
 
   /*
@@ -14,7 +16,10 @@ void cpu_mp_wait (void)
 
   while ((volatile int32_t)cpu_mp_synchro)
   {
-	dna_printf(".");
+	cpu_io_write(UINT32,0x1000,MP_SYNCHRO_WAIT);
+//	dna_printf(".");
   }
+  
+  dna_printf("CPU-%d in cpu_mp_wait Finished!\n", cpu_mp_id());
 }
 
